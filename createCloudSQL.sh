24f6f41fd3c6556ae3be6fc43d451347a574e898
Lans <52331372+lans-repos@users.noreply.github.com>
2019-08-24 22:17:44 -0600

    Create createCloudSQL.sh

---
+++ createCloudSQL.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "This script auotmates the creation of CloudSQL Instance & database on GCP"
+
+read -p "Enter Chosen Name for the CloudSQL Instance: "  instanceName
+read -p "Enter Chosen Database Name: "  databaseName
+read -p "Enter root password for CloudSQL Instance: "  passWord
+
+
+gcloud sql instances create $instanceName --activation-policy=ALWAYS --tier=db-n1-standard-1
+
+gcloud sql databases create $databaseName --instance $instanceName
+
+gcloud sql users set-password root --host=% --instance $instanceName --password=$passWord
+
+exec "$@"
